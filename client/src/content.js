40b70fdaae74dcf4611894ddac58e94155593472
Jens Jaeger <jaegerj204@googlemail.com>
2017-07-30 09:53:03 +0200

    add news property showFull

--- client/src/content.js
+++ client/src/content.js
@@ -52,4 +52,7 @@ const feed = [
     }
 ]
 
+// add view property: show news item with full details: default false
+const view = feed.map(item => ({...item, showFull: false}));
+
 export default feed;
