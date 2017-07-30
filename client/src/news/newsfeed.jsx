da3b5e70f233a5b90014bea7426c4ce3a47decc6
Jens Jaeger <jaegerj204@googlemail.com>
2017-07-30 11:26:17 +0200

    enable react eslint settings and fix more errors

--- client/src/news/newsfeed.jsx
+++ client/src/news/newsfeed.jsx
@@ -1,21 +1,21 @@
-import React from 'react';
+import React from "react";
 import {connect} from "react-redux";
 import NewsItem from "./newsitem";
 
 const mapStateToProps = (state) => state;
 
 const showContentAction = (newsItem) => ({
-        type: "SHOW_CONTENT",
-        news: newsItem
-    });
+    type: "SHOW_CONTENT",
+    news: newsItem
+});
 
 const mapDispathToProps = (dispatch) => ({
     switchContent: (item) => () => dispatch(showContentAction(item))
 });
 
 const list = (news, action) => news.map((item) => <NewsItem key={item.id}
-                                            news={item}
-                                            switchContent={action} />);
+    news={item}
+    switchContent={action} />);
 
 const NewsFeed = ({news, switchContent}) => (
     <div>
